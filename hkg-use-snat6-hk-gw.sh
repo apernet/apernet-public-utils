12aca628a9666599697e5e1b9e8a0310a51288f8
Tony Lee <asli@vip.qq.com>
2021-12-06 01:25:07 +1100

    Create hkg-use-snat6-hk-gw.sh

---
+++ hkg-use-snat6-hk-gw.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+[ "$EUID" -ne 0 ] && {
+    echo "need root." >&2
+    exit 1
+}
+
+(ip -6 addr | grep -q 2406:4440::) && gw=2406:4440::6359:1
+(ip -6 addr | grep -q 2406:4440:0:101:) && gw=2406:4440:0:101::6359:1
+(ip -6 addr | grep -q 2406:4440:0:102:) && gw=2406:4440:0:102::6359:1
+(ip -6 addr | grep -q 2406:4440:0:103:) && gw=2406:4440:0:103::6359:1
+
+[ -z "$gw" ] && {
+    echo "??? IPv6 disabled / not on apernet-hkg." >&2
+    exit 1
+}
+
+ip -6 route change default via $gw       # in case the user is using static gw
+ip -6 route add default via $gw metric 1 # in case the user is using slaac
