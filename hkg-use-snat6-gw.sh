1861d4cf846e0278b8e3ecceee175c8e83d25e35
Honghao Zeng <nat@nat.moe>
2022-08-06 22:59:30 -0700

    hkg-use-snat6-gw.sh: added v104

--- hkg-use-snat6-gw.sh
+++ hkg-use-snat6-gw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 [ "$EUID" -ne 0 ] && {
-    echo "need root." >&2
+    echo "needs root." >&2
     exit 1
 }
 
@@ -9,6 +9,7 @@
 (ip -6 addr | grep -q 2406:4440:0:101:) && gw=2406:4440:0:101::6359
 (ip -6 addr | grep -q 2406:4440:0:102:) && gw=2406:4440:0:102::6359
 (ip -6 addr | grep -q 2406:4440:0:103:) && gw=2406:4440:0:103::6359
+(ip -6 addr | grep -q 2406:4440:0:104:) && gw=2406:4440:0:104::6359
 
 [ -z "$gw" ] && {
     echo "??? IPv6 disabled / not on apernet-hkg." >&2
